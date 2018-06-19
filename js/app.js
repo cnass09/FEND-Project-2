f9643506edc93be9a2bfd0ca48284df6ecf6d7b0
Calista Nasser <nasser.calista@gmail.com>
2018-06-19 18:14:48 -0400

    Added match check functionality

--- js/app.js
+++ js/app.js
@@ -36,6 +36,24 @@ deck.addEventListener('click', event => {
   function addToggleCard(clickTarget) {
   	toggledCards.push(clickTarget);
   }
+
+ /*
+  * If the list already has another card, check to see if the two cards match
+  */
+
+  function matchCheck() {
+  	if (
+  		toggledCards[0].firstElementChild.card === toggledCards[1].firstElementChild.card
+  		) {
+  		console.log('We have a match!');
+  		} else {
+  		console.log('No match. Try again.');
+  		}
+
+  }
+
+  matchCheck();
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -61,10 +79,6 @@ function shuffle(array) {
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
